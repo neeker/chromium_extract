ead3fcd032a456a1e51bf251076de09bdce17ef0
neeker <neeker@gmail.com>
2016-04-22 13:33:49 +0800

    修改文件字符集编码为UTF8。

--- src/third_party/boringssl/src/crypto/cmac/cmac.c
+++ src/third_party/boringssl/src/crypto/cmac/cmac.c
@@ -1,4 +1,4 @@
-/* ====================================================================
+﻿/* ====================================================================
  * Copyright (c) 2010 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
