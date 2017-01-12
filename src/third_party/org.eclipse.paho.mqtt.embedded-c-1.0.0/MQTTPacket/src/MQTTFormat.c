1c170099f279c21959fb59651ae271328cc4cd52
neeker <neeker@gmail.com>
2017-01-12 14:05:43 +0800

    fix snprintf

--- src/third_party/org.eclipse.paho.mqtt.embedded-c-1.0.0/MQTTPacket/src/MQTTFormat.c
+++ src/third_party/org.eclipse.paho.mqtt.embedded-c-1.0.0/MQTTPacket/src/MQTTFormat.c
@@ -19,6 +19,14 @@
 
 #include <string.h>
 
+#if defined(_MSC_VER)
+#if _MSC_VER < 1900
+#define snprintf _snprintf
+#endif
+#if _MSC_VER < 1500
+#define vsnprintf(b,c,f,a) _vsnprintf(b,c,f,a)
+#endif
+#endif
 
 const char* MQTTPacket_names[] =
 {
